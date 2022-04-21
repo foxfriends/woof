7b55600a9ef78150d5c23f0b4a0d9c0da028a313
Cameron Eldridge <cameldridge@gmail.com>
2022-04-21 00:11:41 -0400

    Add an export

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod rest_model;
 mod traits;
 
 pub use error::{Error, Result};
+pub use extractors::PrimaryKey;
 pub use pagination::{CursorPagination, LimitOffsetPagination, PageNumberPagination};
 pub use rest_model::RestModel;
 pub use traits::{Create, Filter, Rest, Update};
